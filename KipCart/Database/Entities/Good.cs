1089b2980c15ae76e2a7c3c9f458b8b464a1e78e
TitaniumLexa <titanium.lexa@gmail.com>
2024-06-18 19:06:02 +0300

    Функциональность части "Покупка"

--- KipCart/Database/Entities/Good.cs
+++ KipCart/Database/Entities/Good.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
 
 namespace KipCart.Database.Entities
 {
@@ -6,7 +9,7 @@ namespace KipCart.Database.Entities
     /// Сущность товара
     /// </summary>
     [PrimaryKey(nameof(ID))]
-    public class Good
+    public class Good:INotifyPropertyChanged
     {
         /// <summary>
         /// Идентификатор товара
@@ -22,5 +25,29 @@ namespace KipCart.Database.Entities
         /// Показывать в списке товаров
         /// </summary>
         public bool IsShow { get; set; }
+
+        [NotMapped]
+        public bool Show
+        {
+            get
+            {
+                return IsShow;
+            }
+            set
+            {
+                if (IsShow != value)
+                {
+                    IsShow = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
---
+++ KipCart/ViewModels/GoodsViewModel.cs
@@ -0,0 +1,79 @@
+﻿using KipCart.Database;
+using KipCart.Database.Entities;
+using KipCart.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace KipCart.ViewModels
+{
+    public class GoodsViewModel
+    {
+        private readonly KipCartContext _context;
+        private readonly IMessagesService _messagesService;
+
+        public ObservableCollection<Good> Goods { get; set; }
+        public ICollectionView FilteredView { get; set; }
+        public Good? SelectedGood { get; set; }
+
+        private readonly ICommand _addToPurchaseCommand;
+        public ICommand AddToPurchaseCommand
+        {
+            get
+            {
+                return _addToPurchaseCommand;
+            }
+        }
+
+        private readonly ICommand _openCatalogWindowCommand;
+        public ICommand OpenCatalogWindowCommand
+        {
+            get
+            {
+                return _openCatalogWindowCommand;
+            }
+        }
+
+        public GoodsViewModel(KipCartContext context, IMessagesService messagesService)
+        {
+            _context = context;
+            _messagesService = messagesService;
+
+            _addToPurchaseCommand = new RelayCommand(AddToPurchase);
+            _openCatalogWindowCommand = new RelayCommand(OpenCatalogWindow);
+
+            _context.Goods.Load();
+            Goods = _context.Goods.Local.ToObservableCollection();
+
+            var collectionViewsSource = new CollectionViewSource { Source = Goods, IsLiveFilteringRequested = true };
+            collectionViewsSource.LiveFilteringProperties.Add(nameof(Good.Show));
+
+            FilteredView = collectionViewsSource.View;
+            FilteredView.Filter = element =>
+            {
+                var good = element as Good;
+                return good?.Show ?? false;
+            };
+        }
+
+        private void AddToPurchase(object? parameter)
+        {
+            if (SelectedGood != null)
+            {
+                _messagesService.SendMessage("AddToPurchase", SelectedGood);
+            }
+        }
+        private void OpenCatalogWindow(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
